2b9104bedbd3d10e1500c392891d9d5590853791
Dyah Ayu Mustikaning Tyas <dyahayumt@Dyahs-MacBook-Air.local>
2018-02-19 11:59:43 +0700

    add my file

--- src/prime.js
+++ src/prime.js
@@ -1,14 +1,14 @@
-function prime(x) {
-  var min = 2;
-  while (min <= x) {
-    if (x % min == 0) {
-      x /= min;
-    } else {
-      min++;
+function isPrime(x) {
+     var min = 2;
+     while (min <= x) {
+         if (x % min == 0) {
+             x /= min;
+         } else {
+             min++;
+            }
     }
-  }
     return min;
-}
-module.exports = prime;
+ }
+ module.exports = isPrime;
 
 
