fbe0081b9e977ae3a1803f164e6576a33f835c67
Dyah Ayu Mustikaning Tyas <dyahayumt@Dyahs-MacBook-Air.local>
2018-02-19 12:12:52 +0700

    commit

--- test/prime.test.js
+++
@@ -1,5 +0,0 @@
-const prime = require('../src/prime');
-
-test('Greatest prime factors from 600851475143', () => {
-  expect(prime(600851475143)).toBe(6857);
-});
--- test/prime2.test.js
+++
@@ -1,5 +0,0 @@
-const prime = require('../src/prime');
-
-test('Greatest prime factors from 13195', () => {
-  expect(prime(13195)).toBe(29);
-});
